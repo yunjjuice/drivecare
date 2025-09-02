667510df301b24ef90c7d90487e82c554bab2d04
최윤주 <yjchoi311@gitauto.com>
2025-09-02 09:19:14 +0900

    init project

---
+++ drivecare/src/main/java/com/gitauto/drivecare/DrivecareApplication.java
@@ -0,0 +1,13 @@
+package com.gitauto.drivecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivecareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivecareApplication.class, args);
+    }
+
+}
---
+++ drivecare/src/test/java/com/gitauto/drivecare/DrivecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitauto.drivecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivecareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
