87bbcc00add2d82ca526d0b8c31ea38a8c6b51d2
정유진(240015) <jungy3530@gitauto.com>
2025-10-27 21:49:04 +0900

    fix: 정비 리스트 조회 API 내 토큰 검증 추가

--- drivecare/src/main/java/com/gitauto/drivecare/api/controller/ReservationRestController.java
+++ drivecare/src/main/java/com/gitauto/drivecare/api/controller/ReservationRestController.java
@@ -1,10 +1,14 @@
 package com.gitauto.drivecare.api.controller;
 
+import com.gitauto.drivecare.api.dto.ApiResponse;
 import com.gitauto.drivecare.api.dto.ReservationResponseDto;
 import com.gitauto.drivecare.api.service.ReservationService;
+import com.gitauto.drivecare.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +20,13 @@ import java.util.List;
 public class ReservationRestController {
 
     private final ReservationService reservationApiService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/list")
-    public ResponseEntity<List<ReservationResponseDto>> getReservationList() {
-        // TODO : 로그인 토큰 생기면 토큰 이용하여 해당 유저 데이터만 조회할 수 있도록 변경 필요
-        return ResponseEntity.ok().body(reservationApiService.reservationList());
+    public ResponseEntity<ApiResponse<List<ReservationResponseDto>>> getReservationList(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        String userId = jwtTokenProvider.getSubject(token);
+
+        return ResponseEntity.ok(ApiResponse.ok(reservationApiService.reservationList(userId)));
     }
 }
