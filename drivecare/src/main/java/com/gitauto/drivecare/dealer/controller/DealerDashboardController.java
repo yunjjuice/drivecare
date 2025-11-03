bd5fccbe5337a9d5a0b88148c52fcb4a92411bb7
최윤주 <yjchoi311@gitauto.com>
2025-11-03 20:27:43 +0900

    feat: 딜러 대시보드 데이터 표출, 미구현 기능 화면 내 미표출

--- drivecare/src/main/java/com/gitauto/drivecare/dealer/controller/DealerDashboardController.java
+++ drivecare/src/main/java/com/gitauto/drivecare/dealer/controller/DealerDashboardController.java
@@ -1,15 +1,24 @@
 package com.gitauto.drivecare.dealer.controller;
 
 import com.gitauto.drivecare.database.user_info.entity.UserInfoEntity;
+import com.gitauto.drivecare.dealer.dto.DealerDashboardDto;
+import com.gitauto.drivecare.dealer.service.DealerDashboardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
+@RequiredArgsConstructor
 public class DealerDashboardController {
+
+    private final DealerDashboardService dealerDashboardService;
+
     @GetMapping("/dealer/dashboard")
     public String viewDashboard(@SessionAttribute("loginUser") UserInfoEntity loginUser, Model model) {
+        DealerDashboardDto dashboardData = dealerDashboardService.getTodayReservationsCount(loginUser);
+        model.addAttribute("dashboard", dashboardData);
         return "dealer/dashboard";
     }
 }
